0a85fb8d11f369a1ad3dcfe9632e944852e0ce64
shin1rok <skssikm@gmail.com>
2018-11-29 01:41:29 +0900

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :job_offers
   resources :companies
+  # root 'pages#index'
+  root 'companies#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
