d381e87ed600bbc5f649a3df1e873e8f89d957c8
shin1rok <skssikm@gmail.com>
2018-11-29 08:16:03 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_160451) do
+ActiveRecord::Schema.define(version: 2018_11_28_164350) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "address", null: false
     t.string "email", null: false
     t.string "tell", null: false
-    t.integer "employees", null: false
+    t.integer "employees", default: 0, null: false
     t.decimal "capital", precision: 11
     t.date "establish_date", null: false
     t.string "url", null: false
@@ -33,6 +33,58 @@ ActiveRecord::Schema.define(version: 2018_11_28_160451) do
     t.integer "status", default: 0, null: false
     t.integer "public_status", default: 2, null: false
     t.index ["company_id"], name: "index_job_offers_on_company_id"
+  end
+
+  create_table "job_seekers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_job_seekers_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_job_seekers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_job_seekers_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_job_seekers_on_unlock_token", unique: true
+  end
+
+  create_table "recruiters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_recruiters_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_recruiters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_recruiters_on_unlock_token", unique: true
   end
 
   add_foreign_key "job_offers", "companies"
