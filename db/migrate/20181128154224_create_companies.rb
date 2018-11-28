7b40a74585620063982fe5a596ffba4ce8f36873
shin1rok <skssikm@gmail.com>
2018-11-29 00:46:10 +0900

    companyのマイグレーションファイル修正

--- db/migrate/20181128154224_create_companies.rb
+++ db/migrate/20181128154224_create_companies.rb
@@ -1,16 +1,15 @@
 class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies do |t|
-      t.string :name
-      t.string :address
-      t.string :email
-      t.string :tell
-      t.integer :employees
-      t.decimal :capital
-      t.date :establish_date
-      t.string :url
-      t.float :average_age
-
+      t.string :name, null: false, commnet: '会社名'
+      t.string :address, null: false, commnet: '住所'
+      t.string :email, null: false, commnet: 'email'
+      t.string :tell, null: false, commnet: 'tell'
+      t.integer :employees, null: false, 　default: 0, commnet: '従業員数'
+      t.decimal :capital, precision: 11, scale: 0, commnet: '資本金'
+      t.date :establish_date, null: false, commnet: '設立日'
+      t.string :url, null: false, commnet: 'URL'
+      t.float :average_age, null: false, commnet: '平均年齢'
       t.timestamps
     end
   end
