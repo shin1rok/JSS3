a3dbf4c71e42d2832d2217f7026ec8cb361f203c
shin1rok <skssikm@gmail.com>
2018-11-29 02:04:18 +0900

    migrationファイル修正

--- db/migrate/20181128154224_create_companies.rb
+++ db/migrate/20181128154224_create_companies.rb
@@ -5,7 +5,7 @@ class CreateCompanies < ActiveRecord::Migration[5.2]
       t.string :address, null: false, commnet: '住所'
       t.string :email, null: false, commnet: 'email'
       t.string :tell, null: false, commnet: 'tell'
-      t.integer :employees, null: false, 　default: 0, commnet: '従業員数'
+      t.integer :employees, null: false, default: 0, commnet: '従業員数'
       t.decimal :capital, precision: 11, scale: 0, commnet: '資本金'
       t.date :establish_date, null: false, commnet: '設立日'
       t.string :url, null: false, commnet: 'URL'
